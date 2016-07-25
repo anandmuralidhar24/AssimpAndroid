76d7da1abfa29917788046e4d7c61e7e6c35033a
Anand Muralidhar <anand@tryamble.com>
2016-07-25 12:22:39 +0530

    updated assimp build script

--- app/src/main/externals/assimp-3.0/buildForAndroid.sh
+++ app/src/main/externals/assimp-3.0/buildForAndroid.sh
@@ -5,15 +5,12 @@ export ANDROID_NDK=$CODE/android-ndk-r11c
 
 # Points to the Android SDK
 export ANDROID_SDK=$HOME/Android/sdk
-
-# This file must be downloaded from the android-cmake project on github:
-# https://github.com/taka-no-me/android-cmake
-export ANDROID_STANDALONE_TOOLCHAIN=$CODE/android-cmake/android.toolchain.cmake
-
-# Do not touch these
 export PATH=$PATH:$ANDROID_SDK/tools
 export PATH=$PATH:$ANDROID_SDK/platform-tools
 export PATH=$PATH:$ANDROID_SDK/android-toolchain/bin
+
+# from https://github.com/taka-no-me/android-cmake
+export ANDROID_STANDALONE_TOOLCHAIN=$CODE/android-cmake/android.toolchain.cmake
 
 # Add additional args here as appropriate
 cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_STANDALONE_TOOLCHAIN \
@@ -26,7 +23,6 @@ cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_STANDALONE_TOOLCHAIN \
       ..
 
 # This is to remove the versioned shared libs in assimp.
-# Might be useful for other CMake-based projects, otherwise remove this line.
 sed -i s/-soname,libassimp.so.3/-soname,libassimp.so/g code/CMakeFiles/assimp.dir/link.txt
 
 #make -j4
